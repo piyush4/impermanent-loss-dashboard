d102f0f03c47f71bd29c50e3653f760b887a465c
piyush4 <piyushawasthi6559@gmail.com>
2022-04-21 19:03:33 +0530

    adding http headers

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -7,5 +7,6 @@ export const upArrow = 0x2191
 export const downArrow = 0x2193
 
 export const headers = {
-    headers:{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers:{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'},
+    referrer:'https://lcd-osmosis.blockapsis.com/swagger/'
 }
